c7892ac270bf3a8b40e50f49d57c46328bd85966
useradd1980 <137459604+useradd1980@users.noreply.github.com>
2026-09-13 16:54:22 -0600

    Sync persisted character data on login and clone

--- src/main/java/dev/cobbledeep/character/CharacterEvents.java
+++ src/main/java/dev/cobbledeep/character/CharacterEvents.java
@@ -1,8 +1,9 @@
 package dev.cobbledeep.character;
 
-import net.minecraft.server.level.ServerPlayer;
 import dev.cobbledeep.Cobbledeep;
+import dev.cobbledeep.network.RPGNetwork;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -14,10 +15,7 @@ import net.minecraftforge.fml.common.Mod;
 public class CharacterEvents
 {
     private static final ResourceLocation CHARACTER_DATA_ID =
-            ResourceLocation.fromNamespaceAndPath(
-                    Cobbledeep.MODID,
-                    "character_data"
-            );
+            ResourceLocation.fromNamespaceAndPath(Cobbledeep.MODID, "character_data");
 
     @SubscribeEvent
     public static void attachCapabilities(AttachCapabilitiesEvent<Entity> event)
@@ -25,30 +23,35 @@ public class CharacterEvents
         if (event.getObject() instanceof Player)
         {
             CharacterDataProvider provider = new CharacterDataProvider();
-
-            event.addCapability(
-                    CHARACTER_DATA_ID,
-                    provider
-            );
-
+            event.addCapability(CHARACTER_DATA_ID, provider);
             event.addListener(provider::invalidate);
         }
     }
 
     @SubscribeEvent
+    public static void onPlayerClone(PlayerEvent.Clone event)
+    {
+        event.getOriginal().reviveCaps();
+        event.getOriginal().getCapability(CharacterCapabilities.CHARACTER_DATA).ifPresent(oldData ->
+                event.getEntity().getCapability(CharacterCapabilities.CHARACTER_DATA).ifPresent(newData ->
+                        newData.copyFrom(oldData)));
+        event.getOriginal().invalidateCaps();
+    }
+
+    @SubscribeEvent
     public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event)
     {
-        if (!(event.getEntity() instanceof ServerPlayer serverPlayer))
-            return;
+        if (!(event.getEntity() instanceof ServerPlayer serverPlayer)) return;
 
-        serverPlayer
-                .getCapability(CharacterCapabilities.CHARACTER_DATA)
-                .ifPresent(data ->
-                {
-                    Cobbledeep.LOGGER.info(
-                            "Character created: {}",
-                            data.isCharacterCreated()
-                    );
-                });
+        serverPlayer.getCapability(CharacterCapabilities.CHARACTER_DATA).ifPresent(data ->
+        {
+            Cobbledeep.LOGGER.info(
+                    "Cobbledeep character loaded: created={}, name={}, race={}, class={}",
+                    data.isCharacterCreated(),
+                    data.getName(),
+                    data.getRace(),
+                    data.getCharacterClass());
+            RPGNetwork.sendCharacterData(serverPlayer, data);
+        });
     }
 }
