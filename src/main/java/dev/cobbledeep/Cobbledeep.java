afb2dbd2de3045a13cae77d36e00727b95e476cf
useradd1980 <137459604+useradd1980@users.noreply.github.com>
2026-09-19 16:03:24 -0600

    Wire giant rat into Cobbledeep startup

--- src/main/java/dev/cobbledeep/Cobbledeep.java
+++ src/main/java/dev/cobbledeep/Cobbledeep.java
@@ -2,6 +2,7 @@ package dev.cobbledeep;
 
 import com.mojang.logging.LogUtils;
 
+import dev.cobbledeep.monster.GiantRatRegistration;
 import dev.cobbledeep.network.RPGNetwork;
 import dev.cobbledeep.registry.ModMenus;
 import net.minecraftforge.fml.common.Mod;
@@ -18,6 +19,7 @@ public class Cobbledeep
     public Cobbledeep(FMLJavaModLoadingContext context)
     {
         ModMenus.MENUS.register(context.getModEventBus());
+        GiantRatRegistration.register(context.getModEventBus());
         RPGNetwork.register();
 
         LOGGER.info("Cobbledeep loaded.");
